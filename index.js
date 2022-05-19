83b459f06b00dc29a1f8a6ec6589af0bef661000
Taemin Kim <taeminkimny@gmail.com>
2022-05-19 16:42:19 -0400

    commit tree 4a4af366ad68348ad8e49474dd5e216e0505631c

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+const arr = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+
+function findMatching(arr, string) {
+    return arr.filter(driver => 
+        driver.toLowerCase() === string.toLowerCase() 
+)};
+
+function fuzzyMatch(arr, string) {
+    return arr.filter(driver => driver.split('')[0] === string.split('')[0])
+}
+
+function matchName(arr, string) {
+    return arr.filter(driver => driver.name === string)
+}
+
+
